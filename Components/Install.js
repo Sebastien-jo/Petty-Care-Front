d65ec4b047a2152cfa35275562fbde2860a5cbfd
eaccipe <eaccipe@gmail.com>
2022-06-28 19:39:22 +0200

    Clean du fichier Install

--- Components/Install.js
+++ Components/Install.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View,TouchableOpacity,Image,SafeAreaView,FlatList , ScrollView, } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image, FlatList} from 'react-native';
+import Style from '../Style';
 
 const Install = ({navigation}) => {
-
-
-
 
     const DATA = [
         {
@@ -21,76 +19,49 @@ const Install = ({navigation}) => {
         },
       ];
 
-      const renderItem = ({ item }) => (
+    const renderItem = ({item}) => (
         <View>
-
-        <Text style={{lineHeight : 25, fontSize : 15, paddingLeft : 10, padding : 10,}}>{item.title}</Text>
+            <Text style={{lineHeight: 25, fontSize: 15, paddingLeft: 10, padding: 10}}>{item.title}</Text>
         </View>
-      );
-    return(
-        
+    );
+
+    return(       
         <View style={styles.Install}>
             
             <View>
-            <Image style={{width : 161.55, borderRadius : 99,  alignSelf : "center",}}  source={require("./ImgRad.png")} />  
-               <Image source={require("../assets/Text.png")}  style={{width : 138, height : 31,marginVertical : 10,alignSelf : "center"}}  />
+                <Image style={{width: 161.55, borderRadius: 99, alignSelf: "center"}} source={require("./ImgRad.png")} />
+
+                <Image source={require("../assets/Text.png")} style={{width: 138, height: 31, marginVertical: 10, alignSelf: "center"}}/>
             </View>
             
-            <View style={{flex : 2}}>
+            <View style={{flex: 2}}>
 
-            <Text style={{ fontSize : 20, color : "#0F4E4E",padding :10}}>Lançons-nous ! Voici ce que vous devez faire avant de commencer :</Text>
-            <FlatList style={{paddingVertical : 50}}
+            <Text style={Style.primaryTitle}>Lançons-nous ! Voici ce que vous devez faire avant de commencer :</Text>
+
+            <FlatList style={{paddingVertical: 50}}
                 data={DATA}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
-                />
-                </View>
+            />
             
-                
-                
-                <TouchableOpacity onPress={() => navigation.navigate("CGU")}  style={styles.ButtonB}>
-                            <Text style={styles.ButtonTextB}>Continuer</Text>
-                </TouchableOpacity>
-              
-                
+            </View>
+            
+            <TouchableOpacity onPress={() => navigation.navigate("CGU")} style={Style.primaryCta}>
+                <Text style={Style.primaryCtaText}>Continuer</Text>
+            </TouchableOpacity>
+                          
         </View>
     )
 }
 
 export default Install
 
-
 const styles = StyleSheet.create({
-    Install : {
-        flex : 1,
-        alignItems : "center",
-        justifyContent : "space-between",
-       
-        textAlign : "center",
-        marginVertical : 10
-       
-    },
-  
- 
-      ButtonTextB: {
-        fontSize: 16,
-        color: "#ffff",
-        alignSelf: "center",
-        textTransform: "uppercase",
-        lineHeight : 21,
-      
-      },
-      ButtonB : {
-        elevation: 1,
-        backgroundColor: "#0F4E4E",
-        borderRadius: 18,
-        paddingVertical: 14,
-        paddingHorizontal: 10,
-        marginVertical : 32,
-        width : 300,
-        marginLeft : "auto",
-        marginRight : "auto"
-         
-      },
-
+    Install: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        textAlign: "center",
+        marginVertical: 10    
+    }
 })
