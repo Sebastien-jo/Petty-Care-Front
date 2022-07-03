aaf1a1511e4d8e7c40ef48c5a7abb31bd953e847
eaccipe <eaccipe@gmail.com>
2022-07-03 14:03:23 +0200

    clean et edit page App

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet} from 'react-native';
 import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Install from "./Components/Install"
 import Home from './Components/Home'
@@ -11,27 +11,20 @@ import CGU from "./Components/CGU"
 import Login from "./Components/Login"
 import Confidential from "./Components/Confidential"
 import TabNavigator from "./TabNavigator "
-import Welcome from "./Components/Welcome"
+
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-   <NavigationContainer>
-    <Stack.Navigator >
-           <Stack.Screen name=" " component={Home}  />
-           <Stack.Screen name="Install" component={Install}   />
-           <Stack.Screen name="CGU" component={CGU}   />
-           <Stack.Screen name="Confidential" component={Confidential}   />
-           <Stack.Screen name="Connexion" component={Login}   />
-           <Stack.Screen name="Tab" component={TabNavigator} options={{ headerShown: false}}  />
-           
-           
-    </Stack.Navigator>
-   </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name=" " component={Home}/>
+                <Stack.Screen name="Install" component={Install}/>
+                <Stack.Screen name="CGU" component={CGU}/>
+                <Stack.Screen name="Confidential" component={Confidential}/>
+                <Stack.Screen name="Connexion" component={Login}/>
+                <Stack.Screen name="Tab" component={TabNavigator} options={{headerShown: false}}/>          
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
