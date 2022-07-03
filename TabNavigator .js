7adf07047b20c4ceaf053c4cc12c8f570130ab7f
eaccipe <eaccipe@gmail.com>
2022-07-03 14:24:19 +0200

    edit Tapbar

--- TabNavigator .js
+++ TabNavigator .js
@@ -1,17 +1,33 @@
-import { View, Text,Image } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Image, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from "@react-navigation/native";
 
-import Welcome from "./Components/Welcome"
+import Welcome from "./Components/Welcome";
 
 const Tab = createBottomTabNavigator();
+
 const TabNavigator = () => {
-    return <Tab.Navigator>
-        <Tab.Screen name='Journal' component={Welcome} options={{headerShown: false,   tabBarIcon: () => (<Image source={require("./assets/HomeIcon.png")} style={{width: 30, height: 30}} />)}} />
-        <Tab.Screen name='Profil' component={Welcome} options={{headerShown: false,   tabBarIcon: () => (<Image source={require("./assets/CatIcon.png")} style={{width: 30, height: 30}} />)}} />
-        <Tab.Screen name='Jouer' component={Welcome} options={{headerShown: false,   tabBarIcon: () => (<Image source={require("./assets/JouerIcon.png")} style={{width: 30, height: 30}} />)}} />
-        <Tab.Screen name='Compte' component={Welcome} options={{headerShown: false,   tabBarIcon: () => (<Image source={require("./assets/CompteIcon.png")} style={{width: 30, height: 30}} />)}} />
-    </Tab.Navigator>
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Journal" component={Welcome} options={{headerShown: false, tabBarIcon: () => (<Image source={require("./assets/Tap_Icon_Home.png")} style={styles.contentTab} />)}} />
+
+                <Tab.Screen name="Profil" component={Welcome} options={{headerShown: false, tabBarIcon: () => (<Image source={require("./assets/Tap_Icon_Cat.png")} style={styles.contentTab} />)}} />
+
+                <Tab.Screen name="Jouer" component={Welcome} options={{headerShown: false, tabBarIcon: () => (<Image source={require("./assets/Tap_Icon_Play.png")} style={styles.contentTab} />)}} />
+
+                <Tab.Screen name="Compte" component={Welcome} options={{headerShown: false, tabBarIcon: () => (<Image source={require("./assets/Tap_Icon_Account.png")} style={styles.contentTab} />)}} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }
 
-export default TabNavigator 
+const styles = StyleSheet.create ({
+    contentTab: {
+        width: 30,
+        height: 30
+    }
+})
+
+export default TabNavigator
