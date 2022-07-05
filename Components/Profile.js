9014a8f8bf7e161d8742c7f566b316f52266cb3d
eaccipe <eaccipe@gmail.com>
2022-07-05 10:24:32 +0200

    modifications des formulaires User et Animal

--- Components/Profile.js
+++ Components/Profile.js
@@ -3,9 +3,10 @@ import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
 import Style from "../Style";
 
 const Profile = ({navigation}) => {
-    const [username, setUsername] = useState("") //input username
-    const [age, setAge] = useState("") // input age
-    const [weight, setWeight] = useState("") // input poids
+    const [username, setUsername] = useState("")
+    const [datebirth, setDatebirth] = useState("")
+    const [weight, setWeight] = useState("")
+    const [goalWeight, setGoalWeight] = useState("")
 
     return (
         <View style={Style.container}>
@@ -16,18 +17,27 @@ const Profile = ({navigation}) => {
 
             <View style={{width: "100%"}}>
                 <View>
-                    <Text style={Style.currentText}>Prénom de l'animal</Text>
+                    <Text style={Style.currentText}>Prénom de l'animal*</Text>
                     <TextInput value={username} onChangeText={(username) => setUsername(username)} placeholder='Son prénom' style={Style.labelText}/>   
                 </View>
 
                 <View>
-                    <Text style={Style.currentText}>Âge de l'animal</Text>
-                    <TextInput value={age} onChangeText={(age) => setAge(age)} placeholder='Son âge' style={Style.labelText}/>
+                    <Text style={Style.currentText}>Date de naissance</Text>
+                    <TextInput value={datebirth} onChangeText={(datebirth) => setDatebirth(datebirth)} placeholder='Sa date de naissance' style={Style.labelText}/>
                 </View>
 
                 <View>
                     <Text style={Style.currentText}>Poids de l'animal</Text>
                     <TextInput value={weight} onChangeText={(weight) => setWeight(weight)} placeholder='Son poids' style={Style.labelText}/>
+                </View>
+
+                <View>
+                    <Text style={Style.currentText}>Poids à atteindre</Text>
+                    <TextInput value={goalWeight} onChangeText={(goalWeight) => setGoalWeight(goalWeight)} placeholder='Son poids idéal' style={Style.labelText}/>
+                </View>
+
+                <View>
+                    <Text style={Style.baselineText}>* Champ obligatoire</Text>
                 </View>
             </View>
 
