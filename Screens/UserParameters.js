34b5b28e4284d2736d71198a94a189eeb5a99243
eaccipe <eaccipe@gmail.com>
2022-07-11 17:57:51 +0200

    ajout scroll UserParameters

--- Screens/UserParameters.js
+++ Screens/UserParameters.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import {useForm} from "react-hook-form";
 import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import FormInput from '../Components/FormInput';
 import {UserProfileContext} from "../Context/UserProfileContext";
 import Style from "../Style";
@@ -24,74 +25,76 @@ const Parameters = () => {
     
     return (
         <View style={Style.container}>
-            <View>
-                <Text style={Style.secondaryTitle}>Profil</Text>
-            </View>
-
-            <View style={Style.headerParameters}>
-                <Text style={Style.tertiaryTitle}>Modifier mon profil</Text>
-                <Image style={{width: "100%", height: 220, borderRadius: 18, marginTop: 10}} source={require("../assets/Image_Profile_User.png")}/>
-            </View>
-
-            <View style={Style.blockParameters}>
-                <Text style={Style.tertiaryTitle}>Mes informations personnelles</Text>
-
+            <ScrollView style={Style.scrollview}>
                 <View>
-                    <Text style={Style.currentText}>Nom</Text>
-                    <FormInput
-                        name="lastname"
-                        placeholder={userProfile.lastname}
-                        control={control}
-                        rules={{required: "Veuillez renseigner un nom valide"}}
-                    />
-                </View>                
-
-                <View>
-                    <Text style={Style.currentText}>Prénom</Text>
-                    <FormInput
-                        name="firstname"
-                        placeholder={userProfile.firstname}
-                        control={control}
-                        rules={{required: "Veuillez renseigner un prénom valide"}}
-                    />
+                    <Text style={Style.secondaryTitle}>Profil</Text>
                 </View>
 
-                <View>
-                    <Text style={Style.currentText}>Adresse email</Text>
-                    <FormInput
-                        name="username"
-                        placeholder={userProfile.username}
-                        control={control}
-                        rules={{required: "Veuillez renseigner un email valide"}}
-                    />
+                <View style={Style.headerParameters}>
+                    <Text style={Style.tertiaryTitle}>Modifier mon profil</Text>
+                    <Image style={{width: "100%", height: 220, borderRadius: 18, marginTop: 10}} source={require("../assets/Image_Profile_User.png")}/>
                 </View>
 
-                <View>
-                    <Text style={Style.currentText}>Adresse postale</Text>
-                    <FormInput
-                        name="address"
-                        placeholder={userProfile.address}
-                        control={control}
-                        rules={{required: "Veuillez renseigner une adresse valide"}}
-                    />
+                <View style={Style.blockParameters}>
+                    <Text style={Style.tertiaryTitle}>Mes informations personnelles</Text>
+
+                    <View>
+                        <Text style={Style.currentText}>Nom</Text>
+                        <FormInput
+                            name="lastname"
+                            placeholder={userProfile.lastname}
+                            control={control}
+                            rules={{required: "Veuillez renseigner un nom valide"}}
+                        />
+                    </View>                
+
+                    <View>
+                        <Text style={Style.currentText}>Prénom</Text>
+                        <FormInput
+                            name="firstname"
+                            placeholder={userProfile.firstname}
+                            control={control}
+                            rules={{required: "Veuillez renseigner un prénom valide"}}
+                        />
+                    </View>
+
+                    <View>
+                        <Text style={Style.currentText}>Adresse email</Text>
+                        <FormInput
+                            name="username"
+                            placeholder={userProfile.username}
+                            control={control}
+                            rules={{required: "Veuillez renseigner un email valide"}}
+                        />
+                    </View>
+
+                    <View>
+                        <Text style={Style.currentText}>Adresse postale</Text>
+                        <FormInput
+                            name="address"
+                            placeholder={userProfile.address}
+                            control={control}
+                            rules={{required: "Veuillez renseigner une adresse valide"}}
+                        />
+                    </View>
+
+                    <View style={Style.submitCenter}>
+                        <TouchableOpacity onPress={handleSubmit} style={Style.primaryCta}>
+                            <Text style={Style.primaryCtaText}>Enregistrer les modifications</Text>
+                        </TouchableOpacity>
+                    </View>          
                 </View>
 
-                <View style={Style.submitCenter}>
-                    <TouchableOpacity onPress={handleSubmit} style={Style.primaryCta}>
-                        <Text style={Style.primaryCtaText}>Enregistrer les modifications</Text>
+                <View style={Style.optionActions}>
+                    <TouchableOpacity onPress={() => navigation.navigate("")} style={Style.tertiaryCta}>
+                        <Text style={Style.tertiaryCtaText}>Historique de commande</Text>
                     </TouchableOpacity>
-                </View>          
-            </View>
 
-            <View style={Style.optionActions}>
-                <TouchableOpacity onPress={() => navigation.navigate("")} style={Style.tertiaryCta}>
-                    <Text style={Style.tertiaryCtaText}>Historique de commande</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => navigation.navigate("")}>
-                    <Text style={Style.alertLink}>Supprimer mon compte</Text>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity onPress={() => navigation.navigate("")}>
+                        <Text style={Style.alertLink}>Supprimer mon compte</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </View>
     )
 }
